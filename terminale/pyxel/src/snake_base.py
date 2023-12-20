5ec8f093223fbb0c69167b2558bea2fbdbabc882
DEMERVILLE Erwan <demerville@hotmail.fr>
2023-12-20 08:18:55 +0100

    Deployed 3969be9 with MkDocs version: 1.5.3

--- terminale/pyxel/src/snake_base.py
+++ terminale/pyxel/src/snake_base.py
@@ -3,14 +3,23 @@ from random import randint
 
 class SnakeGame:
     def __init__(self):
-        pass
+        
+        # AJOUTEZ ICI VOS INSTRUCTIONS
+        
+        # exécuter le jeu (cette instruction doit rester à la fin du constructeur)
+        pyxel.run(self.update, self.draw)
 
     def update(self):
+        
         pass
 
     def draw(self):
+        
+        # on ajoute une instruction permettant d'effacer l'écran
+        # et de le remplir avec la couleur 0 (= couleur noire) :
+        pyxel.cls(0)
+        
         pass
 
 if __name__ == "__main__":
-    game = SnakeGame()  # Création d'un objet de type SnakeGame
-    pyxel.run(game.update, game.draw)  # Exécution du jeu
+    SnakeGame()  # Instanciation de la classe SnakeGame
