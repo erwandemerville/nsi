91e5070b70d6bab9625298d721291a184f835f56
Erwan Demerville <demerville@hotmail.fr>
2024-10-08 18:40:26 +0200

    MAJ site NSI

--- docs/terminale/recursivite/scripts/03.py
+++
@@ -1,7 +0,0 @@
-def factorielle(n):
-    ''' Définition itérative de la fonction factorielle. '''
-    pass
-
-def factorielle_rec(n):
-    ''' Définition récursive de la fonction factorielle. '''
-    pass
